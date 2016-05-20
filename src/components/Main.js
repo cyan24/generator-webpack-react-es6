4008fa5e1595cac966ee9454d8a9398f750b16ea
cyan24 <554958201@qq.com>
2016-05-21 01:14:53 +0800

    add loginPage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,43 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/Login.css');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
-
 class AppComponent extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+    	username : '',
+		pwd : '',
+    };
+  }
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+     <form className='form' onSubmit={this.submitHandler.bind(this)}>
+		<div className='ipt'>
+			<input  name='username' type='text' placeholder='用户名' onChange={this.handleChange.bind(this, 'username')} />
+		</div>
+		<div className='ipt' >
+			<input name='pwd' type='password'  placeholder='密码' onChange={this.handleChange.bind(this, 'pwd')} />
+		</div>
+		<div className='btn'>
+			<input className='bt' value="login" type="submit"/>
+		</div>
+		<div className='tips'>这是一个recat登录demo</div>
+	</form>
     );
   }
+	submitHandler(event) {
+		event.preventDefault();
+		console.log(this.state);
+	}
+	handleChange (name,event) {
+		var newState = {};
+		newState[name] = event.target.value;
+		this.setState(newState);
+	}
 }
+
 
 AppComponent.defaultProps = {
 };
